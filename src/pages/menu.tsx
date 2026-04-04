0fc669e4295e5b49b2b48b8fafae349ad3f44ca2
Wilkerylr <lopezrewilker12@gmail.com>
2026-04-04 13:56:59 -0400

    inhabilitacion de como amfitrion y modo jugador en la pantalla

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -17,7 +17,7 @@ export default function Menu() {
                     </svg>
                     Iniciar partida
                 </Link>
-                <Link to="/anfitrion" className="menu-btn-secundario">
+                {/*<Link to="/anfitrion" className="menu-btn-secundario">
                     <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                         <rect x="3" y="3" width="18" height="18" rx="2" ry="2" />
                         <line x1="9" y1="9" x2="15" y2="15" />
@@ -35,6 +35,7 @@ export default function Menu() {
                 </Link>
                 <Link to="/config" className="menu-btn-secundario">Configuración</Link>
                 <Link to="/personalizartema" className="menu-btn-secundario">Personalizar temas</Link>
+                */}
             </div>
 
             <BotonAyuda />
